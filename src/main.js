824c4dfc2b79e983f2cd49a7fdca575e4c3887f4
amir <amirtaki623@gmail.com>
2025-05-19 22:58:43 +0330

    js : getData

--- src/main.js
+++ src/main.js
@@ -33,7 +33,17 @@ const calculateAge = () => {
         d3 = d2 -d1;
     }
     else {
-
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 - d1
     }
+    if (m3 < 0){
+        m2 = 11;
+        y3-- ;
+    }
+
+    console.log(y3, m3, d3)
 }
 
+function getDaysInMonth (year, month){
+    return new Date(year, month, 0).getDate()
+}
